959e62bc3ab43eead39366d15f8505f11757fc66
John Casey <casey@galatea-associates.com>
2016-07-12 14:47:24 -0400

    added basic conversation handling flow

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -36,6 +36,7 @@ intents = {
 # List of users for the bot to ignore
 user_ignore_list = ['USLACKBOT']
 
+conversation_intent_types = []
 
 class RtmEventHandler(object):
     def __init__(self, slack_clients, msg_writer):
@@ -54,6 +55,7 @@ class RtmEventHandler(object):
             'delete-drive-file': (delete_drive_file, "delete filename"),
             # 'send-email': (send_email, "hello person@galatea-associates.com"),
         }
+        self.conversations = set()
 
     def handle(self, event):
 
@@ -80,43 +82,31 @@ class RtmEventHandler(object):
 
     def _handle_message(self, event):
 
-        # Event won't have a user if slackbot is unfurling messages for you
-        if 'user' not in event:
-            return
-
-        # Filter out messages from the bot itself
-        if self.clients.is_message_from_me(event['user']):
+        if not self._proof_message(event):
             return
 
         msg_txt = event['text']
         channel_id = event['channel']
 
-        # Filter out message unless this bot is mentioned or it is a direct message
-        if not (is_direct_message(channel_id) or self.clients.is_bot_mention(msg_txt)):
-            return
-
         # Remove mention of the bot so that the rest of the code doesn't need to
         msg_txt = self.clients.remove_mention(msg_txt).strip()
-
-        # Ensure that we don't go to wit with messages posted by an ignored user
-        if event['user'] in user_ignore_list:
-            return
-
-        # bot_uid = self.clients.bot_user_id()
 
         # Ask wit to interpret the text and send back a list of entities
         logger.info("Asking wit to interpret| {}".format(msg_txt))
         wit_resp = self.wit_client.interpret(msg_txt)
 
+        # Add username and channel name to the event object
         user_name = self.clients.get_user_name_from_id(event['user'])
         if is_direct_message(channel_id):
             channel_name = "Direct Message"
         else:
             channel_name = self.clients.get_channel_name_from_id(channel_id)
         event.update({"user_name": user_name, "channel_name": channel_name})
-        # Find the intent with the highest confidence that met our default threshold
+
+        # Initialize self.gala_store if this is the first message
         self._initialize_galastats(event)
 
+        # Find the intent with the highest confidence that met our default threshold
         intent_entity = get_highest_confidence_entity(wit_resp['entities'], 'intent')
 
         # If we couldn't find an intent entity, let the user know
@@ -125,10 +115,60 @@ class RtmEventHandler(object):
             return
 
         intent_value = intent_entity['value']
+        if intent_value in conversation_intent_types:
+            match = self._conversation_match(intent_value, wit_resp, event)
+            if match:
+                event.add({"conversation": match})
+
         if intent_value in self.intents:
-            self.intents[intent_value][0](self.msg_writer, event, wit_resp['entities'])
+            self._conversations_update(self.intents[intent_value][0](self.msg_writer, event, wit_resp['entities']))
         else:
             raise ReferenceError("No function found to handle intent {}".format(intent_value))
+
+    def _proof_message(self, event):
+        # Event won't have a user if slackbot is unfurling messages for you
+        if 'user' not in event:
+            return False
+
+        # Filter out messages from the bot itself
+        if self.clients.is_message_from_me(event['user']):
+            return False
+
+        msg_txt = event['text']
+        channel_id = event['channel']
+
+        # Filter out message unless this bot is mentioned or it is a direct message
+        if not (is_direct_message(channel_id) or self.clients.is_bot_mention(msg_txt)):
+            return False
+
+        # Ensure that we don't go to wit with messages posted by an ignored user
+        if event['user'] in user_ignore_list:
+            return False
+
+        return True
+
+    def _conversation_match(self, intent, wit_resp, event):
+        possible_matches = []
+        for conversation in self.conversations:
+            if conversation['waiting_for'] == intent:
+                possible_matches.append(conversation)
+        if not possible_matches:
+            return
+        elif len(possible_matches) == 1:
+            return possible_matches[0]
+        else:
+            # do something to account for multiple matches
+            return possible_matches[0]
+
+    def _conversations_update(self, conversation):
+        if conversation:
+            found = False
+            for old_conv in self.conversations:
+                if old_conv['id'] == conversation['id']:
+                    self.conversations.remove(old_conv)
+                    self.conversations.add(conversation)
+            if not found:
+                self.conversations.add(conversation)
 
     # Initializes galastats upon the first user message event
     def _initialize_galastats(self, event):
